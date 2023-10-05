0ed390fd9907e0fffc2cc2cba505b89ecd8c8e09
Tony O Gara <tony.ogara@gmail.com>
2023-10-05 21:35:38 +0100

    adding FreeCrmArtId

---
+++ FreeCrmArtId/src/main/java/FreeCrmGrpId/FreeCrmArtId/App.java
@@ -0,0 +1,13 @@
+package FreeCrmGrpId.FreeCrmArtId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
