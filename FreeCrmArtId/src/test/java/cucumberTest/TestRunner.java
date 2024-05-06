44f98b3cbd94c5013d2784e7fad0d1496ff85668
Tony O Gara <tony.ogara@gmail.com>
2024-05-06 13:14:22 +0100

    fix the issue with Imports not being resolved

--- FreeCrmArtId/src/test/java/cucumberTest/TestRunner.java
+++ FreeCrmArtId/src/test/java/cucumberTest/TestRunner.java
@@ -1,14 +1,13 @@
 package cucumberTest;
 
-import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
 
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
 
 
 @RunWith(Cucumber.class)
