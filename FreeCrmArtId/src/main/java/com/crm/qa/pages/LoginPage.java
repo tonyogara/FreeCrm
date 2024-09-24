3267a2e570d5a4096c490c67acd39561e0a420cc
Tony O Gara <tony.ogara@gmail.com>
2024-09-24 10:39:24 +0100

    added space in LoginPage to see how git diff example@

--- FreeCrmArtId/src/main/java/com/crm/qa/pages/LoginPage.java
+++ FreeCrmArtId/src/main/java/com/crm/qa/pages/LoginPage.java
@@ -21,12 +21,12 @@ public class LoginPage extends TestBase
 	
 	public HomePage login(String lgn,String pwd)
 	{	
-		//driver.findElement(By.xpath("//header/div[1]/nav[1]/div[2]/div[1]/div[2]/ul[1]/a[1]")).click();
-		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/header[1]/div[1]/nav[1]/div[2]/div[1]/a[1]/span[1]")).click();
-				
+		
+		driver.findElement(By.xpath("/html[1]/body[1]/div[1]/header[1]/div[1]/nav[1]/div[2]/div[1]/a[1]/span[1]")).click();		
 		driver.findElement(By.xpath("//body[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/input[1]")).sendKeys(lgn);
 		driver.findElement(By.xpath("//body[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/input[1]")).sendKeys(pwd);
 		driver.findElement(By.xpath("//body[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[3]")).click();
+		
 		
 		return new HomePage();
 	}
